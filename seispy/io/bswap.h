e938004ff05a432722ff9d13f93e8a8cd185827a
Joseph Capriotti <josephrcapriotti@gmail.com>
2025-03-06 01:08:33 -0700

    re-fix

--- seispy/io/bswap.h
+++ seispy/io/bswap.h
@@ -20,7 +20,7 @@
 
 #else
 
-#ifdef DHAVE___BUILTIN_BSWAP16
+#ifdef HAVE___BUILTIN_BSWAP16
 #define spy_bswap_u16(x) __builtin_bswap16(x)
 #else
 static inline uint16_t
@@ -30,7 +30,7 @@ spy_bswap_u16(uint16_t x)
 }
 #endif
 
-#ifdef DHAVE___BUILTIN_BSWAP32
+#ifdef HAVE___BUILTIN_BSWAP32
 #define spy_bswap_u32(x) __builtin_bswap32(x)
 #else
 static inline uint32_t
@@ -41,7 +41,7 @@ spy_bswap_u32(uint32_t x)
 }
 #endif
 
-#ifdef DHAVE___BUILTIN_BSWAP64
+#ifdef HAVE___BUILTIN_BSWAP64
 #define spy_bswap_u64(x) __builtin_bswap64(x)
 #else
 static inline uint64_t
@@ -157,15 +157,15 @@ static inline void swap_struct_big_and_system(char *str, size_t *offsets, size_t
     uint16_t *t16;
     uint32_t *t32;
     uint64_t *t64;
-    for(i=0; i < n_attr; ++i, str += *(++offsets), ++sizes){ // *sizes++ gets the value pointed to by size, then increments the sizes pointer
+    for(i=0; i < n_attr; ++i, ++offsets, ++sizes){
         if(*sizes == 2){
-            t16 = (uint16_t *) str;
+            t16 = (uint16_t *) (str + *offsets);
             *t16 = spy_bswap_u16(*t16);
         }else if(*sizes == 4){
-            t32 = (uint32_t *) str;
+            t32 = (uint32_t *) (str + *offsets);
             *t32 = spy_bswap_u32(*t32);
         }else if(*sizes == 8){
-            t64 = (uint64_t *) str;
+            t64 = (uint64_t *) (str + *offsets);
             *t64 = spy_bswap_u64(*t64);
         }
     }
@@ -177,15 +177,15 @@ static inline void swap_struct_little_and_system(char *str, size_t *offsets, siz
     uint16_t *t16;
     uint32_t *t32;
     uint64_t *t64;
-    for(i=0; i < n_attr; ++i, str += *(++offsets), ++sizes){ // *sizes++ gets the value pointed to by size, then increments the sizes pointer
+    for(i=0; i < n_attr; ++i, ++offsets, ++sizes){
         if(*sizes == 2){
-            t16 = (uint16_t *) str;
+            t16 = (uint16_t *) (str + *offsets);
             *t16 = spy_bswap_u16(*t16);
         }else if(*sizes == 4){
-            t32 = (uint32_t *) str;
+            t32 = (uint32_t *) (str + *offsets);
             *t32 = spy_bswap_u32(*t32);
         }else if(*sizes == 8){
-            t64 = (uint64_t *) str;
+            t64 = (uint64_t *) (str + *offsets);
             *t64 = spy_bswap_u64(*t64);
         }
     }
@@ -197,24 +197,27 @@ static inline void swap_struct_pairwise_and_system(char *str, size_t *offsets, s
     uint16_t *t16;
     uint32_t *t32;
     uint64_t *t64;
-    for(i=0; i < n_attr; ++i, str += *(++offsets), ++sizes{ // *sizes++ gets the value pointed to by size, then increments the sizes pointer
+    for(i=0; i < n_attr; ++i, ++offsets, ++sizes){
         if(*sizes == 2){
+            // 2 byte pairswapped is same a little endian?
             #ifdef IS_BIG_ENDIAN
-            t16 = (uint16_t *) str;
+            t16 = (uint16_t *) (str + *offsets);
             *t16 = spy_bswap_u16(*t16);
             #endif
         }else{
-            // Swapping each byte pair gets us to big endian
-            t16 = (*uint16_t) str
+            // First, swap all byte pairs, which would get
+            // us to a big-endian representation:
+            t16 = (*uint16_t) (str + *offsets)
             for(j=0; j<*sizes; j+=2, ++t16){
                 *t16 = spy_bswap_u16(*t16);
             }
+            // Then reverse the bytes if we are little endian and more than two bytes wide
             #ifdef IS_LITTLE_ENDIAN
             if(*sizes == 4){
-                t32 = (uint32_t *) str;
+                t32 = (uint32_t *)  (str + *offsets);
                 *t32 = spy_bswap_u32(*t32);
             }else if{*sizes == 8){
-                t64 = (uint64_t *) str;
+                t64 = (uint64_t *)  (str + *offsets);
                 *t64 = spy_bswap_u64(*t64);
             }
             #endif
