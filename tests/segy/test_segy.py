5f1e0f1eb9443a86fec6c86938a92168b1adc86f
Joseph Capriotti <josephrcapriotti@gmail.com>
2025-03-20 16:25:59 -0600

    get script dir for segy test

--- tests/segy/test_segy.py
+++ tests/segy/test_segy.py
@@ -3,10 +3,12 @@ from pathlib import Path
 from seispy.io.segy_standard import SEGYCollection
 import numpy as np
 
+CURRENT_SCRIPT_DIR = Path(__file__).resolve().parent
+
 @pytest.mark.parametrize('endian', ['big', 'little', 'pairwise'])
 @pytest.mark.parametrize('data_format', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 15, 16])
 def test_segy_data_format_reading(endian, data_format):
-    file = Path('test_files')/f"Format{data_format:02d}{endian}.segy"
+    file = CURRENT_SCRIPT_DIR / Path('test_files') / f"Format{data_format:02d}{endian}.segy"
     if data_format in [7, 15] and endian == 'pairwise':
         pytest.skip('3 byte pairwise numbers are not supported.')
 
